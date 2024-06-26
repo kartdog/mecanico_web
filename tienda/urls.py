686c9341423d2e5b367bfc04022302ea013bc374
Felipe <fe.berrios.ov@gmail.com>
2024-06-26 13:17:14 -0400

    tercer commit, login.
    Se agregó login, logout y sus respectivas páginas.

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Index
     path('', views.index, name = 'index'),
     path('nosotros/', views.nosotros, name = 'nosotros'),
+    # Login
+    path('login/', views.login_user, name = 'login'),
+    path('logout/', views.logout_user, name = 'logout'),
+
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,13 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import *
 
-# Create your views here.
-
+# Inicio
 def index(request):
     productos = Producto.objects.all()
 
     return render(request, 'index.html', {'productos': productos})
 
 def nosotros(request):
-
     return render(request, 'nosotros.html', {})
+
+# Login
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Has iniciado sesión."))
+            return redirect('index')
+        else:
+            messages.success(request, ("Ha ocurrido un error, intenta nuevamente."))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Has cerrado la sesión."))
+    return redirect('index')
