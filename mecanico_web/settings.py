5783238b02aefb6b81eb7f64e8dba5b31e8050d3
Felipe <fe.berrios.ov@gmail.com>
2024-06-28 02:29:29 -0400

    vercel commit, tres.
    Se modificó database, settings.py

--- mecanico_web/settings.py
+++ mecanico_web/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'mecanico_web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'HOST' :  'aws-0-sa-east-1.pooler.supabase.com',
+        'NAME' : 'postgres',
+        'USER' : 'postgres.xdnrpiwvqdvcjrieppbr',
+        'PASSWORD' : 'contraseña#123',
+        'PORT' : '5432',
+        }
 }
 
 
